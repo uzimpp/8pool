3b67cc1ba49d886e7ce18d436a7857aaae399f42
Paruj Ratanaworabhan <paruj.r@gmail.com>
2024-11-14 12:28:51 +0700

    Modify the code to use event-driven simulation.

--- ball.py
+++ ball.py
@@ -2,54 +2,39 @@ import turtle
 import math
 
 class Ball:
-    def __init__(self, size, x, y, vx, vy, color):
+    def __init__(self, size, x, y, vx, vy, color, id):
         self.size = size
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
-        self.mass = 100
         self.color = color
+        self.mass = 100*size**2
+        self.count = 0
+        self.id = id
         self.canvas_width = turtle.screensize()[0]
         self.canvas_height = turtle.screensize()[1]
+        
 
     def draw(self):
-        # draw a circle of radius equals to size at x, y coordinates and paint it with color
+        # draw a circle of radius equals to size centered at (x, y) and paint it with color
         turtle.penup()
         turtle.color(self.color)
         turtle.fillcolor(self.color)
-        turtle.goto(self.x,self.y-self.size)
+        turtle.goto(self.x, self.y-self.size)
         turtle.pendown()
         turtle.begin_fill()
         turtle.circle(self.size)
         turtle.end_fill()
 
-    def move(self, dt):
-        self.x += self.vx*dt
-        self.y += self.vy*dt
-
-    def check_v_wall_collision(self):
-        if abs(self.x) > (self.canvas_width - self.size):
-            return True
-
-    def check_h_wall_collision(self):
-        if abs(self.y) > (self.canvas_height - self.size):
-            return True
-
-    def hitting_v_wall(self):
+    def bounce_off_vertical_wall(self):
         self.vx = -self.vx
+        self.count += 1
 
-    def hitting_h_wall(self):
+    def bounce_off_horizontal_wall(self):
         self.vy = -self.vy
+        self.count += 1
 
-    def distance(self, that):
-        x1 = self.x
-        y1 = self.y
-        x2 = that.x
-        y2 = that.y
-        d = math.sqrt((y2-y1)**2 + (x2-x1)**2)
-        return d
-    
     def bounce_off(self, that):
         dx  = that.x - self.x
         dy  = that.y - self.y
@@ -70,3 +55,69 @@ class Ball:
         self.vy += fy / self.mass
         that.vx -= fx / that.mass
         that.vy -= fy / that.mass
+        
+        # update collision counts
+        self.count += 1
+        that.count += 1
+
+    def distance(self, that):
+        x1 = self.x
+        y1 = self.y
+        x2 = that.x
+        y2 = that.y
+        d = math.sqrt((y2-y1)**2 + (x2-x1)**2)
+        return d
+
+    def move(self, dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
+
+    def time_to_hit(self, that):
+        if self is that:
+            return math.inf
+        dx  = that.x - self.x
+        dy  = that.y - self.y
+        dvx = that.vx - self.vx
+        dvy = that.vy - self.vy
+        dvdr = dx*dvx + dy*dvy
+        if dvdr > 0:
+            return math.inf
+        dvdv = dvx*dvx + dvy*dvy
+        if dvdv == 0:
+            return math.inf
+        drdr = dx*dx + dy*dy
+        sigma = self.size + that.size
+        d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma)
+        # if drdr < sigma*sigma:
+            # print("overlapping particles")
+        if d < 0:
+            return math.inf
+        t = -(dvdr + math.sqrt(d)) / dvdv
+
+        # should't happen, but seems to be needed for some extreme inputs
+        # (floating-point precision when dvdv is close to 0, I think)
+        if t <= 0:
+            return math.inf
+
+        return t
+
+    def time_to_hit_vertical_wall(self):
+        if self.vx > 0:
+            return (self.canvas_width - self.x - self.size) / self.vx
+        elif self.vx < 0:
+            return (self.canvas_width + self.x - self.size) / (-self.vx)
+        else:
+            return math.inf
+
+    def time_to_hit_horizontal_wall(self):
+        if self.vy > 0:
+            return (self.canvas_height - self.y - self.size) / self.vy
+        elif self.vy < 0:
+            return (self.canvas_height + self.y - self.size) / (-self.vy)
+        else:
+            return math.inf
+        
+    def __str__(self):
+        return str(self.x) + ":" + str(self.y) + ":" + str(self.vx) + ":" + str(self.vy) + ":" + str(self.count) + str(self.id)
+    
+        
--- run_ball.py
+++ run_ball.py
@@ -1,11 +1,16 @@
-import turtle
 import ball
+import my_event
+import turtle
 import random
+import heapq
 
 class BouncingSimulator:
     def __init__(self, num_balls):
         self.num_balls = num_balls
         self.ball_list = []
+        self.t = 0.0
+        self.pq = []
+        self.HZ = 4
         turtle.speed(0)
         turtle.tracer(0)
         turtle.hideturtle()
@@ -13,16 +18,32 @@ class BouncingSimulator:
         self.canvas_width = turtle.screensize()[0]
         self.canvas_height = turtle.screensize()[1]
         print(self.canvas_width, self.canvas_height)
-        self.ball_radius = 0.05 * self.canvas_width
+
+        ball_radius = 0.05 * self.canvas_width
         for i in range(self.num_balls):
             x = -self.canvas_width + (i+1)*(2*self.canvas_width/(self.num_balls+1))
             y = 0.0
-            # x = random.uniform(-1*self.canvas_width + self.ball_radius, self.canvas_width - self.ball_radius)
-            # y = random.uniform(-1*self.canvas_height + self.ball_radius, self.canvas_height - self.ball_radius)
-            vx = 4*random.uniform(-1.0, 1.0)
-            vy = 4*random.uniform(-1.0, 1.0)
+            vx = 10*random.uniform(-1.0, 1.0)
+            vy = 10*random.uniform(-1.0, 1.0)
             ball_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            self.ball_list.append(ball.Ball(self.ball_radius, x, y, vx, vy, ball_color))
+            self.ball_list.append(ball.Ball(ball_radius, x, y, vx, vy, ball_color, i))
+
+    # updates priority queue with all new events for a_ball
+    def __predict(self, a_ball):
+        if a_ball is None:
+            return
+
+        # particle-particle collisions
+        for i in range(len(self.ball_list)):
+            dt = a_ball.time_to_hit(self.ball_list[i])
+            # insert this event into pq
+            heapq.heappush(self.pq, my_event.Event(self.t + dt, a_ball, self.ball_list[i]))
+        
+        # particle-wall collisions
+        dtX = a_ball.time_to_hit_vertical_wall()
+        dtY = a_ball.time_to_hit_horizontal_wall()
+        heapq.heappush(self.pq, my_event.Event(self.t + dtX, a_ball, None))
+        heapq.heappush(self.pq, my_event.Event(self.t + dtY, None, a_ball))
     
     def __draw_border(self):
         turtle.penup()
@@ -35,37 +56,52 @@ class BouncingSimulator:
             turtle.left(90)
             turtle.forward(2*self.canvas_height)
             turtle.left(90)
-    
-    def __check_ball_collision(self, i):
-        collision_pairs = []
-        for j in range(len(self.ball_list)):
-            if i == j:
-                continue
-            if self.ball_list[i].distance(self.ball_list[j]) <= self.ball_list[i].size + self.ball_list[j].size:
-                collision_pairs.append([i, j])
-        return collision_pairs
+
+    def __redraw(self):
+        turtle.clear()
+        self.__draw_border()
+        for i in range(len(self.ball_list)):
+            self.ball_list[i].draw()
+        turtle.update()
+        heapq.heappush(self.pq, my_event.Event(self.t + 1.0/self.HZ, None, None))
+
 
     def run(self):
-        dt = 0.5
-        while (True):
-            turtle.clear()
-            self.__draw_border()                
-            for i in range(self.num_balls):
-                self.ball_list[i].draw()
-                self.ball_list[i].move(dt)
-                # the code below will update ball velocities based on colliding conditions
-                if (self.ball_list[i].check_h_wall_collision()):
-                    self.ball_list[i].hitting_h_wall()
-                if (self.ball_list[i].check_v_wall_collision()):
-                    self.ball_list[i].hitting_v_wall()
-                if (len(self.__check_ball_collision(i)) != 0):
-                    for each_collision_pair in self.__check_ball_collision(i):
-                        self.ball_list[each_collision_pair[0]].bounce_off(self.ball_list[each_collision_pair[1]])
+        # initialize pq with collision events and redraw event
+        for i in range(len(self.ball_list)):
+            self.__predict(self.ball_list[i])
+        heapq.heappush(self.pq, my_event.Event(0, None, None))
 
-            turtle.update()
+        while (True):
+            e = heapq.heappop(self.pq)
+            if not e.is_valid():
+                continue
+
+            ball_a = e.a
+            ball_b = e.b
+
+            # update positions, and then simulation clock
+            for i in range(len(self.ball_list)):
+                self.ball_list[i].move(e.time - self.t)
+            self.t = e.time
+
+            if (ball_a is not None) and (ball_b is not None):
+                ball_a.bounce_off(ball_b)
+            elif (ball_a is not None) and (ball_b is None):
+                ball_a.bounce_off_vertical_wall()
+            elif (ball_a is None) and (ball_b is not None):
+                ball_b.bounce_off_horizontal_wall()
+            else:
+                self.__redraw()
+            
+            self.__predict(ball_a)
+            self.__predict(ball_b)
+
+
         # hold the window; close it by clicking the window close 'x' mark
         turtle.done()
 
-num_balls = int(input("Number of balls to simulate: "))
+# num_balls = int(input("Number of balls to simulate: "))
+num_balls = 10
 my_simulator = BouncingSimulator(num_balls)
 my_simulator.run()
